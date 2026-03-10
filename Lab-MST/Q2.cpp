77ad97a68c833c24cb68fed5401882894c3f2261
Dikshay <dikshaycoc@gmail.com>
2026-03-10 12:50:59 +0530

    update q2

--- Lab-MST/Q2.cpp
+++ Lab-MST/Q2.cpp
@@ -1,41 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 #define jack420 ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-#define int long long int
-#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+
 void solve() {
-    // Place your problem-solving logic here.
+
     int n;
-    cin >> n ;
+    cin >> n;
+
     vector<string> v(n);
-    for(int i =0;i<n;i++){
-        string s ;
-        cin >> s;
-        v[i] = s;
+    for(int i = 0; i < n; i++){
+        cin >> v[i];
     }
 
     int ans = INT_MIN;
-    map<int, int > mp;
-    for(auto  i : v){
-        string cur = i;
-        int mask =0;
-        for(auto i : cur){
-            int val = i - 'a';
-            mask |= (1<<val);
+    map<int,int> mp;
+
+    for(auto cur : v){
+        int mask = 0;
+        for(auto c : cur){
+            int val = c - 'a';
+            mask |= (1 << val);
         }
-        if(mp.find(!mask) != mp.end()){
-            ans = max(ans , cur.size() * mp[!mask] );
+        for(auto j : mp){
+            if((j.first & mask) == 0){
+                ans = max(ans, (int)cur.size() * j.second);
+            }
         }
-        mp[mask]= cur.size();
+        mp[mask] = max(mp[mask], (int)cur.size());
     }
-    cout<<(ans== INT_MIN)?-1: ans;
+    cout << (ans == INT_MIN ? -1 : ans);
 }
-int32_t main() {
+
+int main() {
+
+    jack420
+
     int t;
     cin >> t;
-    while (t--) {
+
+    while(t--){
         solve();
         cout << endl;
     }
-    return 0;
 }
